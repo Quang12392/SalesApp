9068768ae24532a326fa443080fd2225b18f489a
KieuHuong <admin@kieuhuong.store>
2026-04-18 16:19:13 +0700

    v57: Better mobile card layout at 768px breakpoint

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v56';
+const CACHE_NAME = 'khs-v57';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=56',
-  './js/app.js?v=56',
-  './js/pos.js?v=56',
+  './css/index.css?v=57',
+  './js/app.js?v=57',
+  './js/pos.js?v=57',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
