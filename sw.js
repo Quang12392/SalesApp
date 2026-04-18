67676fe96496e757c0919ea4751663f19192b0b9
KieuHuong <admin@kieuhuong.store>
2026-04-18 17:51:17 +0700

    v62: Fix POS mobile - padding for cart, force list layout

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v61';
+const CACHE_NAME = 'khs-v62';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=61',
-  './js/app.js?v=61',
-  './js/pos.js?v=61',
+  './css/index.css?v=62',
+  './js/app.js?v=62',
+  './js/pos.js?v=62',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
