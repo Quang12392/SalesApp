76b552dffbade8432352998ab79f69ccd069054d
KieuHuong <admin@kieuhuong.store>
2026-04-19 08:37:33 +0700

    v77: Aggressive PWA auto-update - check on foreground, controllerchange reload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v76';
+const CACHE_NAME = 'khs-v77';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=76',
-  './js/app.js?v=76',
-  './js/pos.js?v=76',
+  './css/index.css?v=77',
+  './js/app.js?v=77',
+  './js/pos.js?v=77',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
