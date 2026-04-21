869de8a7ca371eaa7ac8578b9680bdd1ac19a9fc
KieuHuong <admin@kieuhuong.store>
2026-04-21 08:13:53 +0700

    v107: Flex align date col - date left, buttons right vertical

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v106';
+const CACHE_NAME = 'khs-v107';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=106',
-  './js/app.js?v=106',
-  './js/pos.js?v=106',
+  './css/index.css?v=107',
+  './js/app.js?v=107',
+  './js/pos.js?v=107',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
