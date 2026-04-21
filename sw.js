7f58baa0ec4d1d436f90d62a0b57c46ea46d1e0c
KieuHuong <admin@kieuhuong.store>
2026-04-21 07:20:25 +0700

    v104: Fixed-layout batch table with column percentages

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v103';
+const CACHE_NAME = 'khs-v104';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=103',
-  './js/app.js?v=103',
-  './js/pos.js?v=103',
+  './css/index.css?v=104',
+  './js/app.js?v=104',
+  './js/pos.js?v=104',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
