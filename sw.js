73a5439cbd1772ab51e7317a25dd0c0ba11e01d3
KieuHuong <admin@kieuhuong.store>
2026-04-21 16:56:35 +0700

    v134: Mobile nav flush to bottom with safe-area support

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v133';
+const CACHE_NAME = 'khs-v134';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=133',
-  './js/app.js?v=133',
-  './js/pos.js?v=133',
+  './css/index.css?v=134',
+  './js/app.js?v=134',
+  './js/pos.js?v=134',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
