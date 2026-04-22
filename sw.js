1ade54701e178ed54dc4b0fc451d2c08e3520e7d
KieuHuong <admin@kieuhuong.store>
2026-04-22 09:07:53 +0700

    v142: Wrap long customer names in mobile order cards

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v141';
+const CACHE_NAME = 'khs-v142';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=141',
-  './js/app.js?v=141',
-  './js/pos.js?v=141',
+  './css/index.css?v=142',
+  './js/app.js?v=142',
+  './js/pos.js?v=142',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
