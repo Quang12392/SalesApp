3c4bb0e404e2f672ceb75fbfd9b86edfef348537
KieuHuong <admin@kieuhuong.store>
2026-04-22 16:25:31 +0700

    v161: Allow long product/customer names to wrap instead of truncating

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v160';
+const CACHE_NAME = 'khs-v161';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=160',
-  './js/app.js?v=160',
-  './js/pos.js?v=160',
+  './css/index.css?v=161',
+  './js/app.js?v=161',
+  './js/pos.js?v=161',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
