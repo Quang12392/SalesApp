94ccae80fd5da9b302525f5bfd50ec48aacd7ef8
KieuHuong <admin@kieuhuong.store>
2026-04-22 18:02:29 +0700

    v168: Online text white, dot yellow for green header contrast

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v167';
+const CACHE_NAME = 'khs-v168';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=167',
-  './js/app.js?v=167',
-  './js/pos.js?v=167',
+  './css/index.css?v=168',
+  './js/app.js?v=168',
+  './js/pos.js?v=168',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
