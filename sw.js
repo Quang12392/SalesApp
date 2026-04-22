b70fcebb6417783e5615da58237068c22f0566e5
KieuHuong <admin@kieuhuong.store>
2026-04-22 16:34:36 +0700

    v162: Fix hbar-label text wrapping - override at line 2067 was still truncating

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v161';
+const CACHE_NAME = 'khs-v162';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=161',
-  './js/app.js?v=161',
-  './js/pos.js?v=161',
+  './css/index.css?v=162',
+  './js/app.js?v=162',
+  './js/pos.js?v=162',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
