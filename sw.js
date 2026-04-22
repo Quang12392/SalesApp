fac67f3a629de4b5d8cfd0ad7e49bc8f79b86b87
KieuHuong <admin@kieuhuong.store>
2026-04-22 11:11:34 +0700

    v147: Fix modal scrolling - show Thong bao permission at bottom of role form

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v146';
+const CACHE_NAME = 'khs-v147';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=146',
-  './js/app.js?v=146',
-  './js/pos.js?v=146',
+  './css/index.css?v=147',
+  './js/app.js?v=147',
+  './js/pos.js?v=147',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
