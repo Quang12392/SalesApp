979f7786c0ada1592c8ad633e0c1d9d81d45753e
KieuHuong <admin@kieuhuong.store>
2026-04-23 12:17:17 +0700

    v187: Enlarge toolbar search bars 30% - bigger padding, font, icon

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v186';
+const CACHE_NAME = 'khs-v187';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=186',
-  './js/app.js?v=186',
-  './js/pos.js?v=186',
+  './css/index.css?v=187',
+  './js/app.js?v=187',
+  './js/pos.js?v=187',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
