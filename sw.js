7c8d43911febd71ef06dfcb5afba634f9790746b
KieuHuong <admin@kieuhuong.store>
2026-04-23 11:19:43 +0700

    v183: Fix modal footer buttons overflow on mobile - auto-size to fit

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v182';
+const CACHE_NAME = 'khs-v183';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=182',
-  './js/app.js?v=182',
-  './js/pos.js?v=182',
+  './css/index.css?v=183',
+  './js/app.js?v=183',
+  './js/pos.js?v=183',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
