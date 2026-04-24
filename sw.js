7893afb9a1ffe01a8f0a0653ce1f320558e49f14
KieuHuong <admin@kieuhuong.store>
2026-04-25 05:56:54 +0700

    v218: Bulletproof modal - position:fixed with safe-area top/bottom insets directly on container

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v217';
+const CACHE_NAME = 'khs-v218';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=217',
-  './js/app.js?v=217',
-  './js/pos.js?v=217',
+  './css/index.css?v=218',
+  './js/app.js?v=218',
+  './js/pos.js?v=218',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
