423ce108f4dbf035f8aad6354c02f9ad3385d6f3
KieuHuong <admin@kieuhuong.store>
2026-04-25 06:40:09 +0700

    v224: Modal padding 190px top/bottom

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v223';
+const CACHE_NAME = 'khs-v224';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=223',
-  './js/app.js?v=223',
-  './js/pos.js?v=223',
+  './css/index.css?v=224',
+  './js/app.js?v=224',
+  './js/pos.js?v=224',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
