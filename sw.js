6ab793dc8a79daebb7837e7f1e85b87721b8e9d3
KieuHuong <admin@kieuhuong.store>
2026-04-24 09:52:32 +0700

    v201: Report type buttons 2x2 grid on mobile, no overflow

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v200';
+const CACHE_NAME = 'khs-v201';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=200',
-  './js/app.js?v=200',
-  './js/pos.js?v=200',
+  './css/index.css?v=201',
+  './js/app.js?v=201',
+  './js/pos.js?v=201',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
