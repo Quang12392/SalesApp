afddacc1e81220cf2113a65cc80a6aa81ea697fb
KieuHuong <admin@kieuhuong.store>
2026-04-25 08:09:10 +0700

    v233: PTR indicator red color, moved below content area

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v232';
+const CACHE_NAME = 'khs-v233';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=232',
-  './js/app.js?v=232',
-  './js/pos.js?v=232',
+  './css/index.css?v=233',
+  './js/app.js?v=233',
+  './js/pos.js?v=233',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
