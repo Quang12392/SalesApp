9a56f0af7025d14482cec9adb1745500f71d0404
KieuHuong <admin@kieuhuong.store>
2026-04-25 11:12:54 +0700

    v241: Thuế button full width to match payment row

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v240';
+const CACHE_NAME = 'khs-v241';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=240',
-  './js/app.js?v=240',
-  './js/pos.js?v=240',
+  './css/index.css?v=241',
+  './js/app.js?v=241',
+  './js/pos.js?v=241',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
