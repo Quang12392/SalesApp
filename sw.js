7750aea60c7f302d3b070367cf13ca11300ec1fc
KieuHuong <admin@kieuhuong.store>
2026-04-25 17:23:19 +0700

    v248: Fix Chuyển khoản wrapping - white-space:nowrap + font 0.68rem

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v247';
+const CACHE_NAME = 'khs-v248';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=247',
-  './js/app.js?v=247',
-  './js/pos.js?v=247',
+  './css/index.css?v=248',
+  './js/app.js?v=248',
+  './js/pos.js?v=248',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
