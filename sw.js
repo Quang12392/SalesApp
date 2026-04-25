be9b693a6104cca968a1abd67eea1c6ab04c7ee2
KieuHuong <admin@kieuhuong.store>
2026-04-25 13:00:42 +0700

    v243: Add Thuế Sàn to report - pie chart (red) + payment table

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v242';
+const CACHE_NAME = 'khs-v243';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=242',
-  './js/app.js?v=242',
-  './js/pos.js?v=242',
+  './css/index.css?v=243',
+  './js/app.js?v=243',
+  './js/pos.js?v=243',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
