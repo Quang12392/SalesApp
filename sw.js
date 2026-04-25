18a2e22af6286386fe9039fca097080587f45ba8
KieuHuong <admin@kieuhuong.store>
2026-04-25 11:40:24 +0700

    v242: Add Thuế Sàn as 5th payment method - writes to both col J and N

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v241';
+const CACHE_NAME = 'khs-v242';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=241',
-  './js/app.js?v=241',
-  './js/pos.js?v=241',
+  './css/index.css?v=242',
+  './js/app.js?v=242',
+  './js/pos.js?v=242',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
