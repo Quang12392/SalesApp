02db711e0d5ad550b7a6fc3a390229f3f31c2f9b
KieuHuong <admin@kieuhuong.store>
2026-04-25 17:19:08 +0700

    v247: Payment buttons nowrap + smaller to fit 4 in single row on all phones

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v246';
+const CACHE_NAME = 'khs-v247';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=246',
-  './js/app.js?v=246',
-  './js/pos.js?v=246',
+  './css/index.css?v=247',
+  './js/app.js?v=247',
+  './js/pos.js?v=247',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
