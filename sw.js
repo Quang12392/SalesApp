ebf83300becda0a0047c57d4c233ebd601345e56
KieuHuong <admin@kieuhuong.store>
2026-04-28 05:08:05 +0700

    v254: Report sidebar match mockup - compact, green toggle, red finance, white buttons

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v253';
+const CACHE_NAME = 'khs-v254';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=253',
-  './js/app.js?v=253',
-  './js/pos.js?v=253',
+  './css/index.css?v=254',
+  './js/app.js?v=254',
+  './js/pos.js?v=254',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
