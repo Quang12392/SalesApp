76620e0ccebbab0875e6deea13fb164e030e77c8
KieuHuong <admin@kieuhuong.store>
2026-04-28 05:02:07 +0700

    v253: Report cards colored backgrounds + donut chart green/blue/orange

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v252';
+const CACHE_NAME = 'khs-v253';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=252',
-  './js/app.js?v=252',
-  './js/pos.js?v=252',
+  './css/index.css?v=253',
+  './js/app.js?v=253',
+  './js/pos.js?v=253',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
