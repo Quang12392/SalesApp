88f92ada50787241ce5ede9e44fe1194f6f0cd8e
KieuHuong <admin@kieuhuong.store>
2026-05-12 17:59:00 +0700

    style: darken POS stock labels

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v283';
+const CACHE_NAME = 'khs-v284';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=283',
-  './js/app.js?v=283',
-  './js/pos.js?v=283',
+  './css/index.css?v=284',
+  './js/app.js?v=284',
+  './js/pos.js?v=284',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
