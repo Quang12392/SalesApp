167e72afac149f7d1ec68ce739bdb1c8fe9c3f5c
KieuHuong <admin@kieuhuong.store>
2026-05-13 14:52:06 +0700

    style: badge POS stock on mobile

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v291';
+const CACHE_NAME = 'khs-v292';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=291',
-  './js/app.js?v=291',
-  './js/pos.js?v=291',
+  './css/index.css?v=292',
+  './js/app.js?v=292',
+  './js/pos.js?v=292',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
