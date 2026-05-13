e1495ba2414314c65f1a3dbfb9962399926b9f26
KieuHuong <admin@kieuhuong.store>
2026-05-13 17:36:32 +0700

    style: space POS cart unit price line

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v297';
+const CACHE_NAME = 'khs-v298';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=297',
-  './js/app.js?v=297',
-  './js/pos.js?v=297',
+  './css/index.css?v=298',
+  './js/app.js?v=298',
+  './js/pos.js?v=298',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
