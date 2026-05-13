6eedf7ea13059ea9ee6ccc399a3618ae11d48b19
KieuHuong <admin@kieuhuong.store>
2026-05-13 17:43:19 +0700

    style: keep POS add product bar sticky

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v298';
+const CACHE_NAME = 'khs-v299';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=298',
-  './js/app.js?v=298',
-  './js/pos.js?v=298',
+  './css/index.css?v=299',
+  './js/app.js?v=299',
+  './js/pos.js?v=299',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
