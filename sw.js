2f607450a44e9afb2450f7d442b376cde59fc204
KieuHuong <admin@kieuhuong.store>
2026-05-13 13:46:16 +0700

    style: make add product prompt green

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v288';
+const CACHE_NAME = 'khs-v289';
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './css/index.css?v=288',
-  './js/app.js?v=288',
-  './js/pos.js?v=288',
+  './css/index.css?v=289',
+  './js/app.js?v=289',
+  './js/pos.js?v=289',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
