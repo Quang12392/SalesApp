4f80f7288104461a019178560509bcc8c2d726a6
KieuHuong <admin@kieuhuong.store>
2026-05-20 06:44:59 +0700

    fix: simplify POS add product button

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // Service Worker for QLBH Kieu Huong Store - PWA Offline Support
-const CACHE_NAME = 'khs-v342';
+const CACHE_NAME = 'khs-v343';
 const STATIC_ASSETS = [
   './index.html',
-  './css/index.css?v=342',
-  './js/app.js?v=342',
-  './js/pos.js?v=342',
+  './css/index.css?v=343',
+  './js/app.js?v=343',
+  './js/pos.js?v=343',
   './manifest.json',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png'
